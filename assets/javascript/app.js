0aa34f038b0ce2c66df3620aaff4abce594800aa
Dan Staggs <Dan@Dans-MacBook-Pro.local>
2018-11-18 18:03:33 -0600

    putting timer aside to nail process

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,6 +51,36 @@ function hideStartButton (){
 }
 
 
+
+function renderQuestion(){
+    $("#timer-block").text("Time Remaining: 20 Seconds");
+    $("#question-block").append("<h5>" + questionList[questionHolder].question + "</h5>");
+    for (let i = 0; i < questionList[questionHolder].options.length; i++) {
+        $("#answer-block").append("<input type=radio name=answer-group 'id=answer-radio'>" + questionList[questionHolder].options[i] + "<br>");
+        
+    }
+    
+    activeAnswer = questionList[questionHolder].answer;
+    timer = 20;
+    // countdown();
+}
+
+function nextQuestion(){
+    if (questionHolder === questionList.length){
+        showScorecard()
+    } else {
+        questionHolder++;
+        renderQuestion();
+    }
+};
+
+
+
+function countdownDisplay(){
+    $("#timer-block").text(`Time Remaining: ${timer} seconds`);
+}
+
+
 function newGame(){
     hideStartButton()
 
@@ -62,85 +92,10 @@ function newGame(){
     correctResponse = false;
     renderQuestion()
 }
-
-function renderQuestion(){
-    $("#timer-block").text("Time Remaining: 20 Seconds");
-    $("#question-block").append("<h5>" + questionList[questionHolder].question + "</h5>");
-    for (let i = 0; i < questionList[questionHolder].options.length; i++) {
-        $("#answer-block").append("<input type=radio name=answer-group 'id=answer-radio'>" + questionList[questionHolder].options[i] + "<br>");
-        
-    }
-
-    activeAnswer = questionList[questionHolder].answer;
-    timer = 20;
-    // countdown();
-}
-
-function nextQuestion(){
-    if (questionHolder === questionList.length){
-        showScorecard()
-} else {
-    questionHolder++;
-    renderQuestion();
-}
-};
-
-
-
-function countdownDisplay(){
-    $("#timer-block").text(`Time Remaining: ${timer} seconds`);
-}
-
-
 // click controls
 
-// showStartButton();
-// $(document).on("click", "#start-game-button");
-// $(document).on("click", "#answer-radio")
-//     if ($(this).attr("data-letter") === questionList[questionHolder].answer){
-//         correctResponse = true;
-//     } else {
-//         correctResponse = false;
-//     }
 
-
-// timer import
-
- // Timer start
- function timeStart() {
-    intervalId = setInterval(timeCount, 1000);
-  }
-  
-  // Timer count
-  function timeCount() {
-    if (timer > 0) {
-      timer--;
-      displayTimer();
-    }
-
-    // Check if timer is <= 0
-    if (timer <= 0) {
-      timeStop();
-      correctResponse = false;
-      displayResult();
-    }
-  }
-
-  // Timer stop
-  function timeStop() {
-    clearInterval(intervalId);
-  }
-  
-  // Timer reset
-  function timeReset() {
-    timer = 0;
-  }
-
-  // Timer delay
-  function timeDelay(displayCaller, delayTime) {
-    setTimeout(displayCaller, delayTime);
-  }
-
-  console.log(questionList)
-
-  showStartButton();
+console.log(questionList)
+                                        
+                                        
+                                        
